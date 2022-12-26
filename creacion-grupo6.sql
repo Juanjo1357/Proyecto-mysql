042c3a8666bd21101ec9b5e8be1a3de5b540fb7f
Teresa Abraham <teresa.ahc@gmail.com>
2022-12-26 17:33:08 +0100

    Creación tabla categorias

--- creacion-grupo6.sql
+++ creacion-grupo6.sql
@@ -9,4 +9,12 @@ CONSTRAINT paises_pk PRIMARY KEY (id_pais),
 CONSTRAINT paises_uk1 UNIQUE KEY (nombre_pais)
 );
 
+CREATE TABLE Categorias(
+id_categoria    INT(1),
+genero          VARCHAR(25)     NOT NULL,
+CONSTRAINT categorias_pk PRIMARY KEY (id_categoria),
+CONSTRAINT categorías_uk1 UNIQUE KEY (genero)
+);
+
 SHOW FULL TABLES FROM MundialNatacion2022;
+
