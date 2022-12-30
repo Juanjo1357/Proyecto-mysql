8deabea69b095e3cc3775b02cc10e576444a3358
Teresa Abraham <teresa.ahc@gmail.com>
2022-12-30 12:45:14 +0100

    Creación de todas las tablas restantes

--- creacion-grupo6.sql
+++ creacion-grupo6.sql
@@ -16,5 +16,72 @@ CONSTRAINT categorias_pk PRIMARY KEY (id_categoria),
 CONSTRAINT categorías_uk1 UNIQUE KEY (genero)
 );
 
+CREATE TABLE Medallas(
+id_medalla      INT(1),
+nombre_medalla  VARCHAR(15)     NOT NULL,
+CONSTRAINT medallas_pk PRIMARY KEY (id_medalla),
+CONSTRAINT medallas_uk1 UNIQUE (nombre_medalla)
+);
+
+CREATE TABLE Estilos_nado(
+id_estilo           INT(2),
+nombre              VARCHAR(25)     NOT NULL,
+distancia_metros    INT(3)          NOT NULL,
+CONSTRAINT estilos_nado_pk PRIMARY KEY (id_estilo)
+);
+
+CREATE TABLE Competidores(
+id_competidor       INT(3),
+nombre              VARCHAR(50)     NOT NULL,
+apellido            VARCHAR(50)     NOT NULL,
+id_pais             INT(2)          NOT NULL,
+id_categoria        INT(1)          NOT NULL,
+CONSTRAINT competidores_pk PRIMARY KEY (id_competidor),
+CONSTRAINT competidores_fk1 FOREIGN KEY (id_pais) REFERENCES Paises (id_pais)
+);
+
+CREATE TABLE Equipos(
+id_equipo           INT(1),
+id_competidor       INT(3)          NOT NULL,
+CONSTRAINT equipos_pk PRIMARY KEY (id_equipo),
+CONSTRAINT equipos_uk1 UNIQUE (id_competidor),
+CONSTRAINT equipos_fk1 FOREIGN KEY (id_competidor) REFERENCES Competidores (id_competidor)
+);
+
+CREATE TABLE Clasificacion_individual(
+id_competidor       INT(3)          NOT NULL,
+id_estilo           INT(2),
+id_medalla          INT(1),
+tiempo              TIMESTAMP       NOT NULL,
+CONSTRAINT clasificacion_individual_pk PRIMARY KEY (id_estilo,id_medalla),
+CONSTRAINT clasificacion_individual_fk1 FOREIGN KEY (id_competidor) REFERENCES Competidores (id_competidor),
+CONSTRAINT clasificacion_individual_fk2 FOREIGN KEY (id_estilo) REFERENCES Estilos_nado (id_estilo),
+CONSTRAINT clasificacion_individual_fk3 FOREIGN KEY (id_medalla) REFERENCES Medallas (id_medalla)
+);
+
+CREATE TABLE Clasificacion_grupal(
+id_equipo           INT(3)          NOT NULL,
+id_estilo           INT(2),
+id_medalla          INT(1),
+tiempo_total        TIMESTAMP       NOT NULL,
+CONSTRAINT clasificacion_grupal_pk PRIMARY KEY (id_estilo,id_medalla),
+CONSTRAINT clasificacion_grupal_fk1 FOREIGN KEY (id_equipo) REFERENCES Equipos (id_equipo),
+CONSTRAINT clasificacion_grupal_fk2 FOREIGN KEY (id_estilo) REFERENCES Estilos_nado (id_estilo),
+CONSTRAINT clasificacion_grupal_fk3 FOREIGN KEY (id_medalla) REFERENCES Medallas (id_medalla),
+CONSTRAINT clasificacion_grupal_uk1 UNIQUE (id_equipo)
+);
+
+CREATE TABLE Records (
+fecha               DATE,
+id_estilo           INT(2),
+tiempo              TIMESTAMP,
+id_competidor       INT(3),
+id_pais             INT(2)              NOT NULL,
+CONSTRAINT records_pk PRIMARY KEY (id_estilo, tiempo),
+CONSTRAINT records_fk1 FOREIGN KEY (id_estilo) REFERENCES Estilos_nado (id_estilo),
+CONSTRAINT records_fk2 FOREIGN KEY (id_competidor) REFERENCES Competidores (id_competidor),
+CONSTRAINT records_fk3 FOREIGN KEY (id_pais) REFERENCES Paises (id_pais)
+);
+
 SHOW FULL TABLES FROM MundialNatacion2022;
 
