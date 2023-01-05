1fa526ad5af32860650c1834b2c8acfb76174b0f
Teresa Abraham <teresa.ahc@gmail.com>
2023-01-05 19:15:34 +0100

    Solucionado problema de pk en clasificacion individual, insertados datos para comprobar

--- creacion-grupo6.sql
+++ creacion-grupo6.sql
@@ -57,8 +57,8 @@ CREATE TABLE Clasificacion_individual(
 id_competidor       INT(3)          NOT NULL,
 id_estilo           INT(2),
 id_medalla          INT(1),
-tiempo              TIMESTAMP       NOT NULL,
-CONSTRAINT clasificacion_individual_pk PRIMARY KEY (id_estilo,id_medalla),
+tiempo              TIME(6)       NOT NULL,
+CONSTRAINT clasificacion_individual_pk PRIMARY KEY (id_estilo,id_medalla,id_competidor),
 CONSTRAINT clasificacion_individual_fk1 FOREIGN KEY (id_competidor) REFERENCES Competidores (id_competidor),
 CONSTRAINT clasificacion_individual_fk2 FOREIGN KEY (id_estilo) REFERENCES Estilos_nado (id_estilo),
 CONSTRAINT clasificacion_individual_fk3 FOREIGN KEY (id_medalla) REFERENCES Medallas (id_medalla)
@@ -299,4 +299,24 @@ VALUES (7,18),(7,23),(7,59),(7,43),(7,60),(7,44),
        (9,20),(9,62),(9,55),(9,7),(9,63),(9,64);
 
 Select * FROM Equipos;
+SELECT * FROM Competidores;
+SELECT * FROM Estilos_nado;
+SELECT * FROM Medallas;
+
+SELECT * FROM Clasificacion_individual;
+DELETE FROM Clasificacion_individual where id_estilo=3;
+
+INSERT INTO Clasificacion_individual (id_competidor, id_estilo, id_medalla, tiempo)
+VALUES (1,1,1,('21.32'));
+
+INSERT INTO Clasificacion_individual (id_competidor, id_estilo, id_medalla, tiempo)
+VALUES (2,1,2,('21.41')), (3,1,3,('21.57'));
+
+INSERT INTO Clasificacion_individual (id_competidor, id_estilo, id_medalla, tiempo)
+VALUES (4,2,1,('47.58')), (3,2,2,('47.64')), (5,2,3,('47.71'));
+
+INSERT INTO Clasificacion_individual (id_competidor, id_estilo, id_medalla, tiempo)
+VALUES (4,3,1,('00:01:43.21')), (6,3,2,('00:01:44.47')), (7,3,3,('00:01:44.98'));
+
+
 
